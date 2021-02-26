3cccb1a976cc0ae8ee6e79a4f150e21346b5bbd4
Kevin Hermawan <kevinhermawan1001@gmail.com>
2021-02-26 10:56:42 +0700

    first commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Kevin Hermawan on 26/02/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
