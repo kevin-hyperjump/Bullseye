53c328238392340f935a64569ab31413014d0c7e
Kevin Hermawan <kevinhermawan1001@gmail.com>
2021-03-02 19:22:47 +0700

    Adds dark mode support

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,38 +14,61 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
             
-            Text(String(game.target))
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
-            
-            HStack {
-                Text("1")
+            VStack {
+                Text("Put the Bullseye As Close As You Can To".uppercased())
                     .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .padding(.leading, 30.0)
+                    .padding(.trailing, 30.0)
+                    .foregroundColor(Color("TextColor"))
                 
-                Slider(value: $sliderValue, in: 1.0...100.0)
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("TextColor"))
                 
-                Text("100")
-                    .bold()
-            }
-            
-            Button(action: {
-                self.alertIsVisible = true
-            }) {
-                Text("Hit Me!")
-            }
-            .alert(isPresented: $alertIsVisible) {
-                let roundedValue = Int(self.sliderValue.rounded())
+                HStack {
+                    Text("1")
+                        .bold()
+                        .foregroundColor(Color("TextColor"))
+                    
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    
+                    Text("100")
+                        .bold()
+                        .foregroundColor(Color("TextColor"))
+                }
+                .padding()
                 
-                return Alert(title: Text("Hello There!"), message: Text("The slider's value is \(roundedValue).\n You scored \(self.game.points(sliderValue: roundedValue))"), dismissButton: .default(Text("Awesome!")))
+                Button(action: {
+                    self.alertIsVisible = true
+                }) {
+                    Text("Hit Me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .padding(20.0)
+                .background(
+                    ZStack {
+                        Color("ButtonColor")
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                    }
+                )
+                .foregroundColor(Color.white)
+                .cornerRadius(21.0)
+                .alert(isPresented: $alertIsVisible) {
+                    let roundedValue = Int(self.sliderValue.rounded())
+                    
+                    return Alert(title: Text("Hello There!"), message: Text("The slider's value is \(roundedValue).\n You scored \(self.game.points(sliderValue: roundedValue))"), dismissButton: .default(Text("Awesome!")))
+                }
             }
         }
     }
@@ -54,6 +77,14 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        ContentView().previewLayout(.fixed(width: 568, height: 320))
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+            .preferredColorScheme(.light)
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+            .preferredColorScheme(.dark)
     }
 }
